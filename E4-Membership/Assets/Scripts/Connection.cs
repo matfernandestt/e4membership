4ebae6ae58320e6f67efeff601eade0e810365ce
Mateus Fernandes <matfernandestt@gmail.com>
2020-12-07 23:58:55 -0300

    Initial connection to the database

---
+++ E4-Membership/Assets/Scripts/Connection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Connection : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI connectionText;
+    [SerializeField] private TMP_InputField usernameTextField;
+    [SerializeField] private TMP_InputField imageUrlField;
+    
+    [SerializeField] private Button registerButton;
+
+    [SerializeField] private RawImage profileImage;
+
+    private void Awake()
+    {
+        registerButton.interactable = false;
+        registerButton.onClick.AddListener(Connect);
+    }
+
+    public void Connect()
+    {
+        StartCoroutine(ConnectionCoroutine());
+    }
+
+    private IEnumerator ConnectionCoroutine()
+    {
+        registerButton.interactable = false;
+        WWWForm form = new WWWForm();
+        var randomValue = Random.Range(0, 999999).ToString("000 000");
+        form.AddField("gamercode", randomValue);
+        form.AddField("username", usernameTextField.text);
+        form.AddField("photo", imageUrlField.text);
+        
+        var www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+        if (www.text == "0")
+        {
+            connectionText.text = "SUCCESS";
+        }
+        else
+        {
+            connectionText.text = "ERROR: " + www.text;
+        }
+        SetImage(imageUrlField.text);
+    }
+
+    public void SetImage(string url)
+    {
+        IEnumerator SetImageCoroutine()
+        {
+            WWW www = new WWW(url);
+            yield return www;
+            profileImage.texture = www.texture;
+            www.Dispose();
+        }
+        
+        StartCoroutine(SetImageCoroutine());
+    }
+
+    public void ValidateFields()
+    {
+        registerButton.interactable = (usernameTextField.text.Length > 0 && imageUrlField.text.Length > 0);
+    }
+}
---
+++ E4-Membership/Assets/Scripts/UserData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    public string gamercode;
+    public string username;
+    public string photo;
+}
